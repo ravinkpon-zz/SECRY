d353d99d27ee09a612eb8acfceeb623a217487ba
ALINBABU <alinbabu2010@gmail.com>
2020-04-01 11:41:05 +0530

    initial

--- account/views.py
+++ account/views.py
@@ -121,8 +121,6 @@ def upload_file(request):
             split(myfile)
             key, iv1, iv2, data = generateKey(fileid)
             alnum = enc_order()
-            info = file_info.objects.create(ile_id=fileid, file_name=file_name, user=request.user, file_size=filesize,file_key=key,file_keydata=data)
-            info.save()
             for file in listDir:
                 id = hashlib.sha256(fileid.encode('utf-8')).hexdigest()
                 file_path = os.path.join(dest+'/'+file)
@@ -143,6 +141,9 @@ def upload_file(request):
                 os.remove(file_path)
                 data.save(using=storedb[index])
                 index = index+1
+            info = file_info.objects.create(
+                ile_id=fileid, file_name=file_name, user=request.user, file_size=filesize, file_key=key, file_keydata=data)
+            info.save()
             mail = request.user.email
             name = request.user.first_name
             attach = './media/keys/' + fileid + '.png'
