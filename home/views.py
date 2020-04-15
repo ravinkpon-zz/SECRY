8065b5f5af1dbff397eb6a7d409fe03986efbb01
ALINBABU <alinbabu2010@gmail.com>
2020-04-15 12:45:33 +0530

    final

--- home/views.py
+++ home/views.py
@@ -63,7 +63,7 @@ def register(request):                      #User registration request and proce
             user.save()
             current_site = get_current_site(request)
             mail_subject = 'Activate your SECRY account.'
-            html_message = render_to_string('acc_active_email.html', {'user': user, 'domain': current_site.domain, 'uid': urlsafe_base64_encode(force_bytes(uid)), 'token': account_activation_token.make_token(user)})
+            html_message = render_to_string('acc_active_email.html', {'domain': current_site.domain, 'uid': urlsafe_base64_encode(force_bytes(uid)), 'token': account_activation_token.make_token(user)})
             plain_message = strip_tags(html_message)
             email = EmailMessage(mail_subject, plain_message, 'admin@secrycloud.tech', [email])
             email.send(fail_silently=False)
