2883f65daf25207a95422b493d2bb9bd4d81addf
ALINBABU <alinbabu2010@gmail.com>
2020-04-17 20:39:32 +0530

    final

--- account/views.py
+++ account/views.py
@@ -136,7 +136,7 @@ def upload_file(request):
             name = request.user.first_name
             attach = MEDIA_ROOT + '/keys/' + fileid + '.png'
             mail_subject = 'File Upload'
-            html_message = render_to_string('key_email.html', {'name': name,'fileid':fileid})
+            html_message = render_to_string('key_email.html', {'name': name,'fileid':fileid,'create':1})
             msg = EmailMultiAlternatives(mail_subject, html_message, 'admin@secrycloud.tech', [emailid], reply_to=['admin@secrycloud.tech'], headers={'Message-ID': 'Upload'})
             msg.attach_alternative(html_message, "text/html")
             msg.attach_file(attach)
@@ -353,7 +353,7 @@ def generate(request):                      #Generate the key file for the user.
             attach = MEDIA_ROOT + '/keys/' + id + '.png'
             mail_subject = 'Regenerate Key'
             html_message = render_to_string(
-                'key_email.html', {'name': name, 'fileid': id})
+                'key_email.html', {'name': name, 'fileid': id, 'create': 0})
             msg = EmailMultiAlternatives(mail_subject, html_message, 'admin@secrycloud.tech', [mail], reply_to=['admin@secrycloud.tech'], headers={'Message-ID': 'Upload'})
             msg.attach_alternative(html_message, "text/html")
             msg.attach_file(attach)
