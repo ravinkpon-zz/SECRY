a43e3aa32ba5e2bc98b5ce858a50d89b6fba6622
ALINBABU <alinbabu2010@gmail.com>
2020-04-18 13:56:19 +0530

    final

--- home/views.py
+++ home/views.py
@@ -128,7 +128,7 @@ def change_password(request):           # Password reset function
         user.set_password(password1)
         user.save()
         messages.success(request, "Your passsword has been changed")
-        redirect('signin')
+        return render(request, 'signin.html')
 
 
 def contact(request):                   #Contact form processing
