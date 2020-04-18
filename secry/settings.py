268aa85e9ba2150afd5fb7a6ba6a5af9727ea963
ALINBABU <alinbabu2010@gmail.com>
2020-04-18 13:51:41 +0530

    final

--- secry/settings.py
+++ secry/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 0
+DEBUG = 1
 
 ALLOWED_HOSTS = ["*"]
 
